8ea002b89d426e8811021aea1026e0d46dfa2932
Kris <krzysztofkoz6@gmail.com>
2021-12-30 18:21:47 +0100

    Add basic style/css loader && img loader

--- source/index.js
+++ source/index.js
@@ -1,9 +1,20 @@
-import _ from "lodash";
+import "./style.css";
+import BoxSvg from "./box.svg";
 
 function component() {
-  const element = document.createElement("div");
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  return element;
+  const h1 = document.createElement("h1");
+
+  const boxLogo = new Image();
+  boxLogo.src = BoxSvg;
+  boxLogo.classList.add("logo-svg");
+
+  const paragraph = document.createElement("p");
+  paragraph.textContent = "Hello Webpack";
+
+  h1.appendChild(boxLogo);
+  h1.appendChild(paragraph);
+
+  return h1;
 }
 
 document.body.appendChild(component());
