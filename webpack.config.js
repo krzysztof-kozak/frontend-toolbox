839f7994fe803d64b417836df7dbd6790f249c5e
Kris <krzysztofkoz6@gmail.com>
2022-01-02 16:41:11 +0100

    Add info about potential issue with multiple module instantiation

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,33 @@ module.exports = {
   },
 
   entry: {
+    /*
+ 	Possible gotcha when using multiple entry point per html page:
+
+		[HMR] Update failed: ChunkLoadError: Loading hot update chunk index failed.
+		(missing: http://localhost:8080/someFileName.hash.hot-update.js) 
+
+	
+	Possible solutions:
+
+    - Set runtimeChunk to "single"
+
+		- Don't use multiple entry points:
+
+      Although using multiple entry points per page is allowed in webpack,
+			it should be avoided when possible in favor of an entry point with multiple imports:
+			entry: { page: ['./analytics', './app'] }.
+			This results in a better optimization and consistent execution order when using async script tags.
+
+			SOURCE: https://webpack.js.org/guides/code-splitting/
+
+		Useful links:
+			- https://bundlers.tooling.report/code-splitting/multi-entry/
+			- https://stackoverflow.com/questions/65640449/how-to-solve-chunkloaderror-loading-hot-update-chunk-second-app-failed-in-webpa
+			- https://webpack.js.org/guides/code-splitting/#prevent-duplication
+      - https://webpack.js.org/configuration/optimization/#optimizationruntimechunk
+  */
+
     index: {
       import: "./source/index.js",
       dependOn: "shared",
